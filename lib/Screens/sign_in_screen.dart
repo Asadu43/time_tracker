9b9d416f29ab5f6807f4fcbf6e836970a163241a
BackTrackers43 <backtracker43@gmail.com>
2021-03-01 04:07:19 -0800

    first commit

---
+++ lib/Screens/sign_in_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sign_button/constants.dart';
+import 'package:sign_button/create_button.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign In",
+              style: GoogleFonts.handlee(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SignInButton(
+                buttonType: ButtonType.googleDark,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))
+                ),
+                buttonSize: ButtonSize.large,
+                btnText: "Sign in with Google",
+                onPressed: () {}),
+            SizedBox(height: 10.0,),
+            SignInButton(
+                buttonType: ButtonType.facebookDark,
+                btnText: "Facebook",
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0))
+                ),
+                buttonSize: ButtonSize.large,
+                btnColor: Colors.indigo,
+                onPressed: () {}),
+            SizedBox(height: 10.0,),
+            SignInButton(
+
+              buttonType: ButtonType.mail,
+                btnText: "Sign in with email",
+                buttonSize: ButtonSize.large,
+                imagePosition: ImagePosition.right,
+                btnColor: Colors.teal[700],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))
+                ),
+                onPressed: () {}),
+            SizedBox(height: 15.0,),
+            Text("or",style: TextStyle(fontSize: 15.0,color: Colors.black,),),
+            FlatButton(onPressed: (){},child: Text("Sign in anonymous"),color: Colors.lime[300],minWidth: 290,),
+          ],
+        ),
+      ),
+    );
+  }
+}
